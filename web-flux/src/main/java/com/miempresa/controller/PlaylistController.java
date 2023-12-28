27f30fc53ce1b78ebe90afd5c1ad429e6c65f553
Noonexyz1 <aries-d2github@hotmail.com>
2023-12-28 12:20:50 -0400

    web-flux, Events Streams agregados

--- web-flux/src/main/java/com/miempresa/controller/PlaylistController.java
+++ web-flux/src/main/java/com/miempresa/controller/PlaylistController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 public interface PlaylistController {
     Flux<Playlist> getAllPlaylist();
     Mono<Playlist> getPlayListById(@PathVariable String id);
     Mono<Playlist> saveNewPlaylist(@RequestBody Playlist playlist);
+    Flux<Tuple2<Long, Playlist>> getPlayListByEvents();
 
 }
--- web-flux/src/main/java/com/miempresa/controller/PlaylistControllerImpl.java
+++ web-flux/src/main/java/com/miempresa/controller/PlaylistControllerImpl.java
@@ -3,9 +3,13 @@ package com.miempresa.controller;
 import com.miempresa.document.Playlist;
 import com.miempresa.services.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
 
 @RestController
 public class PlaylistControllerImpl implements PlaylistController {
@@ -29,5 +33,14 @@ public class PlaylistControllerImpl implements PlaylistController {
     @PostMapping(value = "/playlist")
     public Mono<Playlist> saveNewPlaylist(@RequestBody Playlist playlist) {
         return playlistService.save(playlist);
+    }
+
+    @Override
+    @GetMapping(value = "/playlist/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tuple2<Long, Playlist>> getPlayListByEvents() {
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(10));
+        Flux<Playlist> events = playlistService.findAll();
+        System.out.println("Padamos por aqui los eventos");
+        return Flux.zip(interval, events);
     }
 }
